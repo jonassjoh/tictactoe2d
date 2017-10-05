6ab17c270a189c43d4041531691a88616f2ac10b
jonassjoh <jonassjoh@gmail.com>
2017-10-05 15:34:39 +0200

    Implemented pseudocode for alg 1 and alg 2

--- Player.java
+++ Player.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Player {
+
 	/**
 	 * Performs a move
 	 *
@@ -25,7 +26,6 @@ public class Player {
 		 */
    
 		for (GameState state : nextStates) {
-			heuristic(state);
 		}
 
 
@@ -33,7 +33,84 @@ public class Player {
 		return nextStates.elementAt(random.nextInt(nextStates.size()));
 	}
 	
-	public void heuristic(GameState state) {
-		// Insert heuristic function
+	private int minimax(GameState state) {
+		int player = 0;
+		int PLAYER_A = 0;
+		int PLAYER_B = 1;
+		
+		if (mu(state).size() == 0) {
+			return gamma(state);
+		}
+
+
+		boolean player_A = (state.getNextPlayer() == Constants.CELL_X);
+		
+		int bestPossible = player_A ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+		for (GameState child : mu(state)) {
+			int v = minimax(state);
+			bestPossible = player_A ? max(bestPossible, v) : min(bestPossible, v);
+		}
+
+		return bestPossible;
+	}
+
+	/**
+	  * @param state
+	  *		The current state we are analyzing
+	  * @param alpha
+	  *		The current best value achievable by A
+	  * @param beta
+	  *		The current best value achievable by B
+	  * @return
+	  *		The minimax value of the state
+	  */
+	private int alphabeta(GameState state, int depth, int alpha, int beta) {
+
+		if (depth == 0 || mu(state).size() == 0) {
+			return gamma(state);
+		}
+	
+		boolean player_A = (state.getNextPlayer() == Constants.CELL_X);
+		int v = 0;
+
+		if (player_A) {
+			v = Integer.MIN_VALUE;
+			for (GameState child : mu(state)) {
+				v = max(v, alphabeta(child, depth-1, alpha, beta));
+				alpha = max(alpha, v);
+				if (beta <= alpha)
+					break; // beta prune
+			}
+		}
+		else {
+			v = Integer.MAX_VALUE;
+			for (GameState child : mu(state)) {
+				v = min(v, alphabeta(child, depth-1, alpha, beta));
+				beta = min(beta, v);
+				if (beta <= alpha)
+					break; // alpha prune
+			}
+		}
+		return v;
+	}
+
+	private Vector<GameState> mu(GameState state) {
+		Vector<GameState> nextStates = new Vector<GameState>();
+		state.findPossibleMoves(nextStates);
+		return nextStates;
+	}
+
+	private int gamma(GameState state) {
+		// TOOD: Heuristic function
+		return 1;
+	}
+
+	private int min(int a, int b) {
+		return a < b ? a : b;
+	}
+
+	private int max(int a, int b) {
+		return a > b ? a : b;
 	}
 }
