7a63f42b091f5dae1ffcccc2b7290268c2e94546
jonassjoh <jonassjoh@gmail.com>
2017-10-05 14:24:54 +0200

    Inital commit kattio

--- Tictactoe2d.java
+++ Tictactoe2d.java
@@ -1,10 +1,17 @@
 public class Tictactoe2d {
 
+	private Kattio io = new Kattio(System.in, System.out);
+
 	public static void main(String... args) {
 		new Tictactoe2d();
 	}
 
 	public Tictactoe2d() {
-		System.out.println("Hello, world");
+		print("Hello, world\n");
+	}
+
+	private void print(String msg) {
+		io.print(msg);
+		io.flush();
 	}
 }
